7f97f56b02ca81ca89814963facf1d2a2d4b9307
Hernan <plagerhernan@gmail.com>
2019-07-08 12:55:05 -0300

    Nuevo repositorio creado

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+    public static int coinsCount = 0;
+
+	// Use this for initialization
+	void Start () {
+        Coin.coinsCount++;
+		Debug.Log("coinsCount: " + coinsCount);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider) {
+        if (collider.CompareTag("Player"))
+        {
+            //Debug.Log("entrada al trigger");
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    void OnDestroy() {
+        Coin.coinsCount--;
+		Debug.Log("Moneda recogida. " + "coinsCount: " + coinsCount);
+		 
+		//si recojo todas las monedas
+        if (Coin.coinsCount <= 0)
+        {
+			Debug.Log("Ganaste!");
+
+			//destruyo el timer
+			GameObject timer = GameObject.Find ("GameTimer");
+			GameObject.Destroy (timer);
+
+			//activo los fuegos artificiales
+			GameObject[] fuegos = GameObject.FindGameObjectsWithTag ("Finish");
+			foreach (GameObject fuego in fuegos) {
+				fuego.GetComponent<ParticleSystem>().Play ();
+			}
+        }       
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float maxTime = 20.0f;
+	private float _countdown;
+
+	// Use this for initialization
+	void Start () {
+		_countdown = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_countdown -= Time.deltaTime;
+
+		if (_countdown <= 0) {
+			Coin.coinsCount = 0;
+			Debug.Log("Tiempo finalizado. Perdiste!");
+			Application.LoadLevel (Application.loadedLevel);
+
+		}
+	}
+}
